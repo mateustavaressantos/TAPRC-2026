f236e185c0622b74445486210138a178bc7b7499
mateustavaressantos <m76jec@gmail.com>
2026-09-15 20:14:13 -0300

    segundo commit

--- function_app.py
+++ function_app.py
@@ -3,9 +3,20 @@ import azure.functions as func
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
+def teste ():
+    logging.info('Teste')
+
+@app.schedule(schedule="0 */5 * * * *", arg_name="myTimer", run_on_startup=True,
               use_monitor=False) 
 def timer_trigger_mateus(myTimer: func.TimerRequest) -> None:
+    logging.info('Python timer trigger function executed.')
+
+    teste()
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=True,
+              use_monitor=False) 
+def timer_trigger_2(myTimer: func.TimerRequest) -> None:
+    
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
